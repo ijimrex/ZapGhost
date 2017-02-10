ccddbf05c2f36426d62ea4fe39397313ba928d50
MartinTrojans <martinglory1992@gmail.com>
2017-02-09 16:57:32 -0800

    kidding defender 1

--- Zapghost/Assets/Scripts/Defender1.cs
+++ Zapghost/Assets/Scripts/Defender1.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Defender1 : MonoBehaviour {
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		} 
 	}
 }
