6eda4fb8d47e479456fef2a69eb323917201ca84
MartinTrojans <martinglory1992@gmail.com>
2017-02-14 21:02:15 -0800

    Add two scripts draggable, drop zone

---
+++ Zapghost/Assets/Scripts/Draggable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler,IEndDragHandler{
+    public GameObject Defender1;
+    public GameObject d;
+
+    public Transform parentToReturnTo = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("onbegindrag");
+        //parentToReturnTo = this.transform.parent;
+        //this.transform.SetParent(this.transform.parent.parent);
+        //GetComponent<CanvasGroup>().blocksRaycasts = false;
+        d = (GameObject) Instantiate(Defender1, transform.position, transform.rotation);
+    }
+
+   
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("onbegindrag");
+        //this.transform.position = eventData.position;
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x,5,Input.mousePosition.y);
+        Vector3 objPosition= Camera.main.ScreenToWorldPoint(mousePosition);
+        d.transform.position = objPosition;
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("onbegindrag");
+        if (true)
+        {
+            //this.transform.SetParent(parentToReturnTo);
+            //GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+        else
+        {
+            //Destroy(d);
+        }
+    }
+}
---
+++ Zapghost/Assets/Scripts/DropZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour,IDropHandler {
+
+	public void  OnDrop(PointerEventData evenData)
+    {
+        Draggable t = evenData.pointerDrag.GetComponent<Draggable>();
+        if (t != null)
+        {
+            t.parentToReturnTo = this.transform;
+        }
+    }
+}
