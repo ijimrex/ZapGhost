25cf2b37a8f3cd5e7929041e68e0fb4483527d6c
ijimrex <ijimrex@gmail.com>
2017-02-24 19:53:36 -0800

    Merge
    Now can use ui to drag defenders to the grids to defeat monsters.

--- Zapghost/Assets/Scripts/DropZone.cs
+++ Zapghost/Assets/Scripts/DropZone.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour,IDropHandler {
+public class DropZone : MonoBehaviour {
 
-	public void  OnDrop(PointerEventData evenData)
+	public void OnTriggerEnter(Collider collider)
     {
-        Draggable t = evenData.pointerDrag.GetComponent<Draggable>();
+        Draggable t = collider.gameObject.transform.parent.gameObject.GetComponent<Draggable>();
+
         if (t != null)
         {
             t.parentToReturnTo = this.transform;
