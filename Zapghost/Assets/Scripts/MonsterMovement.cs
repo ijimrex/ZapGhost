be8f6a8ce7259c0ee46236ce5a600ad89cc86a90
MartinTrojans <martinglory1992@gmail.com>
2017-04-17 18:58:57 -0700

    Add frozen effect

--- Zapghost/Assets/Scripts/MonsterMovement.cs
+++ Zapghost/Assets/Scripts/MonsterMovement.cs
@@ -4,13 +4,48 @@ using System.Collections;
 public class MonsterMovement : MonoBehaviour
 {
 	public float moveSpeed = 5f;
+	private Color originColor;
+	private float frozenTime;
+
+	void Start() {
+		Renderer[] rds = transform.GetComponentsInChildren<Renderer> ();
+
+		foreach (Renderer r in rds) {
+			foreach (Material m in r.materials) {
+				m.shader = Shader.Find ("Specular");
+				Debug.Log (m.name);
+				originColor = m.GetColor ("_Color");
+			}
+		}
+	}
 
 
 	void Update ()
 	{
-		transform.Translate(new Vector3(0, 0, -1) * moveSpeed * Time.deltaTime);
-	}
-	void onTrigerEnter (Collider other) {
-		Debug.Log (other.name);
+		Renderer[] rds = transform.GetComponentsInChildren<Renderer> ();
+		if (Time.time < frozenTime) {
+			foreach (Renderer r in rds) {
+				foreach (Material m in r.materials) {
+					m.shader = Shader.Find ("Specular");
+					//				Color color = m.GetColor ("_Color");
+					//				Color newColor;
+					//				float grey = Vector3.Dot(new Vector3(color.r, color.g, color.b), new Vector3(0.299f, 0.587f, 0.114f)); 
+					//				newColor = new Color(grey, grey, grey, color.a); 
+					//				newColor.r -= 0.15f; 
+					//				newColor.b += 0.15f; 
+					m.SetColor ("_Color", new Color (0, 83, 246, 255));
+				}
+			}
+
+			transform.Translate (new Vector3 (0, 0, -1) * moveSpeed * 0.7f * Time.deltaTime);
+		} else {
+			foreach (Renderer r in rds) {
+				foreach (Material m in r.materials) {
+					m.shader = Shader.Find ("Specular");
+					m.SetColor ("_Color", originColor);
+				}
+			}
+			transform.Translate (new Vector3 (0, 0, -1) * moveSpeed * Time.deltaTime);
+		}
 	}
 }
