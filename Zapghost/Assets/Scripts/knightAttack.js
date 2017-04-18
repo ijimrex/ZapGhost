5a96ae7fee5eb669bc873444f4b35197e68e6814
Hawk Song <hawksong@outlook.com>
2017-04-17 17:30:50 -0700

    add two new defenders

---
+++ Zapghost/Assets/Scripts/knightAttack.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+private var animationList:Array;
+function Start () {
+     animationList = GetAnimationList();
+}
+
+function Update () {
+  
+     GetComponent.<Animation>().CrossFade(animationList[1],0.01);
+
+}
+
+private function GetAnimationList():Array
+{
+     var tmpArray = new Array();
+     for (var state : AnimationState in gameObject.GetComponent.<Animation>())
+     {
+          tmpArray.Add(state.name);
+     }
+     return tmpArray;
+}
---
+++ Zapghost/Assets/ShortAttackPower.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortAttackPower : MonoBehaviour {
+	public int power = 1000000000; 
+}
