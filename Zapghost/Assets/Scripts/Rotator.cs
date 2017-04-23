fb6e4c117bcfeced48c49274f575759a1e8d40e7
jimrex <ijimrex@gmail.com>
2017-04-22 20:24:58 -0700

    Add level 4

---
+++ Zapghost/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	public float speed;
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime*speed);
+	}
+}
