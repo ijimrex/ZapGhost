0e656a86299ec4f9a1fb86accac7882d5326b925
MartinTrojans <martinglory1992@gmail.com>
2017-04-23 18:25:45 -0700

    Revise main menu script

--- Zapghost/Assets/Scripts/MainMenu.cs
+++ Zapghost/Assets/Scripts/MainMenu.cs
@@ -8,21 +8,27 @@ public class MainMenu : MonoBehaviour {
     public GameObject MenuPanel, AboutPanel;
 
 	public void StartGame(int num) {
+		PlayerPrefs.SetInt("currentLevel", num);
 		string levelName = "Level" + num;
 		SceneManager.LoadScene(levelName);
 	}
 
-	public void PlayAgain() {
-		int currentLevel = GameObject.Find ("HealthSystem").GetComponent<HealthSystem>().level;
-		Debug.Log(currentLevel);
+	public void PlayAgain() {  
+		int currentLevel = PlayerPrefs.GetInt ("currentLevel");
 		string levelName = "Level" + currentLevel;
 		SceneManager.LoadScene(levelName);
 	}
 
 	public void NextLevel() {
-		int nextLevel = GameObject.Find ("HealthSystem").GetComponent<HealthSystem>().level + 1;			
+		Debug.Log (PlayerPrefs.GetInt ("currentLevel"));
+		int nextLevel = PlayerPrefs.GetInt ("currentLevel") + 1;
+		PlayerPrefs.SetInt("currentLevel", nextLevel);
 		string levelName = "Level" + nextLevel;
 		SceneManager.LoadScene(levelName);
+	}
+
+	public void LevelSelect() {
+		SceneManager.LoadScene("LevelSelect");
 	}
 
 
